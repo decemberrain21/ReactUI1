d946368875ad46fe57dfb0039c4501363ed200e5
Nyein Ei Zaw <nyein.eizaw@sierra.sg>
2018-03-14 15:17:41 +0800

    testreactwebpack

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import Navigation from './components/Navigation';
+class App extends Component {
+  render() {
+    return (
+     <div>
+         <Navigation/>
+         <div className="container">
+            <h1>My Blog</h1>
+         </div>
+     </div>
+    );
+  }
+}
+
+export default App;
